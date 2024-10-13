b013ea4e5da1696eaae6bcff21a98bf0e49a09f2
anandhu1202 <86902384+anandhu1202@users.noreply.github.com>
2024-10-13 07:48:11 +0530

    Delete hr_management_system/users directory

--- hr_management_system/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.registerUser, name='signup'),
-    path('', views.loginUser, name='login'),
-    path('logout/', views.loginUser, name='logout')
-]
